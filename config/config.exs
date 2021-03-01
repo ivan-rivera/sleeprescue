bab2de8006398fd258b23a30b8055e98f260704a
ivan.s.rivera@gmail.com <ivan.s.rivera@gmail.com>
2021-03-01 19:07:05 +0100

    cleaned up configs

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,3 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 use Mix.Config
 
 config :sleeprescue,
@@ -13,19 +6,15 @@ config :sleeprescue,
 # Configures the endpoint
 config :sleeprescue, SleeprescueWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "CAwdXRLJtZyjw21BhebnaL88oP1C4Cw+d+tYSKGElwxHanArds8EwmCUoljErBiN",
+  secret_key_base: (System.get_env("SECRET_KEY_BASE_SR") || raise "secret key base is missing"),
   render_errors: [view: SleeprescueWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: Sleeprescue.PubSub,
-  live_view: [signing_salt: "MXHt7iQI"]
+  live_view: [signing_salt: (System.get_env("LIVE_VIEW_SALT_SR") || raise "LV secret is missing")]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
 use Mix.Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :sleeprescue, Sleeprescue.Repo,
   username: "postgres",
   password: "postgres",
@@ -12,11 +7,8 @@ config :sleeprescue, Sleeprescue.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :sleeprescue, SleeprescueWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
