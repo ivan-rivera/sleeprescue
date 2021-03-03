8511fd0dd06e0abf137f29145abd4059fe8edfd5
ivan.s.rivera@gmail.com <ivan.s.rivera@gmail.com>
2021-03-03 19:21:26 +0100

    First touches on the header and footer

--- assets/tailwindcss-config.js
+++ assets/tailwindcss-config.js
@@ -7,6 +7,17 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      dark: '#555B6E',
+      white: "#FAF9F9",
+      primary: "#89B0AE",
+      supplementary: "#BEE3DB",
+      secondary: "#FFD6BA"
+    },
+    fontFamily: {
+      oxygen: ['oxygen', 'sans-serif']
+
+    },
     extend: {},
   },
   variants: {
